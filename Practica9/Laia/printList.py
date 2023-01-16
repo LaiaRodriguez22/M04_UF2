6f0a6740eb28f3a21e8e5e9386e5f26b0509d49c
LaiaRodri <laia.rodriguez@enti.cat>
2023-01-16 19:50:20 +0100

    Exercicis finalitzats

--- Practica9/Laia/printList.py
+++ Practica9/Laia/printList.py
@@ -1,23 +1,29 @@
 areas = ["cuina", 7.88, "menjador", 13.0, "terrassa", 20.34,
          "lavabo", 6.55, "habitació1", 7.98, "habitació2", 12, "rebedor", 4.23]
 
-
+#SEGON element
 print(areas[1])
 
+#ULTIM element
 print(areas[13])
 
-print(areas[4])
+#area de TERRASSA
+print(areas[5])
 
-print(areas[1])
+#del primer element al tercer element
+print(areas[0:4])
 
-print(areas[1])
+#del TERCER element al ULTIM element
+print(areas[3:14])
 
-print(areas[1])
+#Imprimir el total de l'àrea de les dues habitacions
+print(areas[9] + areas[11])
 
-print(areas[1])
+#Modificar l’àrea del lavabo i imprimir la nova list area
+areas[7] = 10.00
+print(areas)
 
-print(areas[1])
-
-print(areas[1])
-
-print(areas[1])
+#Afegir l'àrea de “pati interior” i 2.78 a les últimes posicions. Imprimir la nova list area.
+areas.append("pati interior")
+areas.append(2.78)
+print(areas)
